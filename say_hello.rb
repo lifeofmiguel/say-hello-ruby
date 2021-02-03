59017bcf926a33b9997da15cb64a13e2f45a4f5c
Miguel Angel Morales <mmorales21500@live.com>
2021-02-03 15:29:42 -0500

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,8 +1,3 @@
-# Build your say_hello method here
-puts "Hello, Ruby Programmer!"
+def say_hello ("Gabriela")
+    puts "Hello {"say_hello"} Ruby Programmer!."
   end 
- => :greeting 
-2.6.1 :005 > greeting
-Hello, Ruby Programmer!
- => nil 
-
